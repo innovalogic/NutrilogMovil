917d43058a7c80ab84cbe4d08dea7833d3c00b8b
Amadeo <innovalogicoficial@gmail.com>
2025-05-02 02:16:17 -0400

    Cambios

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
-import "./global.css"
-import { View, Text } from 'react-native';
+import "./global.css";
+import { View, Text, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-400">
-      <Text className="text-blue-700 text-2xl font-extrabold">¡Tailwind está funcionando!</Text>
+    <View className="flex-1 items-center justify-center bg-purple-500">
+      <Text className="text-blue-700 text-2xl font-extrabold mb-8">NUTRILOG</Text>
+      <TouchableOpacity className="bg-blue-600 px-6 py-3 rounded-lg mb-4">
+        <Text className="text-white text-lg font-semibold">Iniciar Sesión</Text>
+      </TouchableOpacity>
+      <TouchableOpacity className="bg-green-600 px-6 py-3 rounded-lg">
+        <Text className="text-white text-lg font-semibold">Registrarse</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
-
-    
   );
 }
