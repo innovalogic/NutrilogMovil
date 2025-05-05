f7980788a28283e5a33fce7bcbaef6c75c060a1c
Amadeo <innovalogicoficial@gmail.com>
2025-05-05 14:52:30 -0400

    Habitos mejora y redireccion a Ejercicio Fisico

---
+++ screens/HabitoEjercicioFisico/CategoriasEjercicioFisico.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CategoriasEjercicioFisico() {
+  return (
+    <View className="flex-1 bg-white justify-center items-center">
+      <Text className="text-2xl font-bold">Categorías de Ejercicio Físico</Text>
+    </View>
+  );
+}
