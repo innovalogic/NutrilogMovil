1f0ba741481bd031fb077035d613bc88ddbb7b2e
Amadeo <amadeosilesdx2@gmail.com>
2025-05-09 21:59:39 -0400

    Cambios en el registro

--- screens/RegistroHabitos/RegisterHabitCategoryScreen.tsx
+++ screens/RegistroHabitos/RegisterHabitCategoryScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
@@ -9,6 +9,7 @@ type RootStackParamList = {
   Nutrition: undefined;
   RegistroEjercicios: undefined;
   Menu:undefined;
+  CategoriasEjercicioFisico: undefined;
 };
 
 type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
@@ -17,38 +18,44 @@ const RegisterHabitCategoryScreen = () => {
   const navigation = useNavigation<NavigationProp>();
 
   return (
-    <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-3xl font-bold text-white mb-8">Iniciemos con tus hábitos</Text>
-
-      {/* Salud Mental */}
-      <TouchableOpacity
-        className="bg-white rounded-xl p-6 mb-5 items-center w-48"
-        onPress={() => navigation.navigate('MentalHealth')}
-      >
-        <Text className="text-lg font-bold">Salud Mental</Text>
-      </TouchableOpacity>
-
-      {/* Alimentación */}
-      <TouchableOpacity
-        className="bg-white rounded-xl p-6 mb-5 items-center w-48"
-        onPress={() => navigation.navigate('Nutrition')}
-      >
-        <Text className="text-lg font-bold">Alimentación</Text>
-      </TouchableOpacity>
-
-      {/* Actividad Física */}
-      <TouchableOpacity
-        className="bg-white rounded-xl p-6 mb-5 items-center w-48"
-        onPress={() => navigation.navigate('RegistroEjercicios')}
-      >
-        <Text className="text-lg font-bold">Actividad Física</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-              className="bg-green-600 py-3 px-8 rounded-md w-64 items-center m-10"
-              onPress={() => navigation.navigate('Menu')}
-            >
-              <Text className="text-white text-lg font-bold">Terminar Registros de Habitos</Text>
-      </TouchableOpacity>
+    <View className="flex-1 bg-[#5F75E4] relative">
+      <View className="items-center p-20">
+        <Text className="text-white text-4xl font-bold">HÁBITOS</Text>
+        <Text className="text-white text-3xl font-medium mt-5">¿Qué hábito deseas adquirir?</Text>
+      </View>
+      <View className="p-4 mt-[-40]">
+        <View className="flex-row justify-around mb-8">
+          <TouchableOpacity
+            className="items-center"
+            onPress={() => navigation.navigate('CategoriasEjercicioFisico')} // Navigate on press
+          >
+            <Image
+              source={require('../../assets/EjercicioFisico.png')}
+              className="w-40 h-40 rounded-full"
+              resizeMode="contain"
+            />
+            <Text className="text-white text-lg font-extrabold mt-7">Ejercicio Físico</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="items-center">
+            <Image
+              source={require('../../assets/Alimentacion.png')}
+              className="w-40 h-40 rounded-full"
+              resizeMode="contain"
+            />
+            <Text className="text-white text-lg font-extrabold mt-7">Alimentación</Text>
+          </TouchableOpacity>
+        </View>
+        <View className="items-center">
+          <TouchableOpacity className="items-center">
+            <Image
+              source={require('../../assets/SaludMental.png')}
+              className="w-40 h-40 rounded-full"
+              resizeMode="contain"
+            />
+            <Text className="text-white text-lg font-extrabold mt-7">Salud Mental</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
