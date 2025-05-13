595924721a0fdae68307a22212ed224c62e6d4ad
Andres Chavez <t4t4x777@gmail.com>
2025-05-13 17:01:05 -0400

    actualizacion de menu

--- screens/RegistroHabitos/RegisterYogaLevelScreen.tsx
+++ screens/RegistroHabitos/RegisterYogaLevelScreen.tsx
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Text, TouchableOpacity, View, Alert, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, doc, getDoc, updateDoc, setDoc, arrayUnion } from 'firebase/firestore';
 import { auth, firestore } from '../../firebase'; // Asegúrate de que la importación sea correcta
+import { StackNavigationProp } from '@react-navigation/stack';
 
 type RootStackParamList = {
   RegistroEjercicios: undefined;
+  Principiante: undefined,
+  Intermedio: undefined,
+  Avanzado: undefined,
 };
+
+type HabitScreenNavigationProp = StackNavigationProp<RootStackParamList>;
 
 const niveles = [
   {
     nombre: 'Principiante',
     descripcion: 'Ideal para quienes comienzan. Aprende posturas básicas y mejora tu flexibilidad.',
-    icono: require('../../assets/yoga1.png')
+    icono: require('../../assets/yoga1.png'),
+    route: 'Yoga',
   },
   {
     nombre: 'Intermedio',
     descripcion: 'Para quienes ya tienen experiencia. Aumenta la dificultad y controla tu respiración.',
     icono: require('../../assets/yoga2.jpg'),
+    route: 'YogaIntermedio',
   },
   {
     nombre: 'Avanzado',
     descripcion: 'Para practicantes experimentados. Desafía tu cuerpo con posturas avanzadas y relajación profunda.',
     icono: require('../../assets/yoga3.jpg'),
+    route: 'YogaAvanzado'
   }
 ];
 
@@ -32,48 +41,58 @@ const RegisterYogaLevelScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   const [selectedLevel, setSelectedLevel] = useState<string | null>(null);
 
-  const handleSave = async () => {
-    if (!selectedLevel) {
-      Alert.alert('Selecciona un nivel antes de guardar.');
-      return;
-    }
+  useFocusEffect(
+    useCallback(() => {
+      setSelectedLevel(null);
+    }, [])
+  );
 
-    try {
-      const user = auth.currentUser;
-
-      if (!user) {
-        Alert.alert('No se encontró un usuario autenticado.');
-        return;
-      }
-
-      const userRef = doc(firestore, 'usuariohabitosactividadfisica', user.uid);
-      const userDoc = await getDoc(userRef);
-
-      if (!userDoc.exists()) {
-        // Si el documento no existe, crearlo
-        await setDoc(userRef, {
-          niveles: arrayUnion({ nivel: selectedLevel, actividad: 'Yoga', timestamp: new Date() }),
-        });
-      } else {
-        // Si el documento existe, actualizarlo
-        await updateDoc(userRef, {
-          niveles: arrayUnion({ nivel: selectedLevel, actividad: 'Yoga', timestamp: new Date() }),
-        });
-      }
-
-      Alert.alert('Nivel guardado exitosamente!');
-      navigation.navigate('RegistroEjercicios');
-    } catch (error) {
-      console.error(error);
-      Alert.alert('Error al guardar el nivel.');
-    }
+  const handleNavigation = (route: keyof RootStackParamList) => {
+    navigation.navigate(route);
   };
 
+  // const handleSave = async () => {
+    // if (!selectedLevel) {
+    //   Alert.alert('Selecciona un nivel antes de guardar.');
+    //   return;
+    // }
+
+    // try {
+    //   const user = auth.currentUser;
+
+    //   if (!user) {
+    //     Alert.alert('No se encontró un usuario autenticado.');
+    //     return;
+    //   }
+
+    //   const userRef = doc(firestore, 'usuariohabitosactividadfisica', user.uid);
+    //   const userDoc = await getDoc(userRef);
+
+    //   if (!userDoc.exists()) {
+    //     // Si el documento no existe, crearlo
+    //     await setDoc(userRef, {
+    //       niveles: arrayUnion({ nivel: selectedLevel, actividad: 'Yoga', timestamp: new Date() }),
+    //     });
+    //   } else {
+    //     // Si el documento existe, actualizarlo
+    //     await updateDoc(userRef, {
+    //       niveles: arrayUnion({ nivel: selectedLevel, actividad: 'Yoga', timestamp: new Date() }),
+    //     });
+    //   }
+
+    //   Alert.alert('Nivel guardado exitosamente!');
+    //   navigation.navigate('RegistroEjercicios');
+    // } catch (error) {
+    //   console.error(error);
+    //   Alert.alert('Error al guardar el nivel.');
+    // }
+  // };
+
   return (
-    <View className="flex-1 justify-center items-center bg-[#595959]">
+    <View className="flex-1 justify-center items-center bg-gray-700">
 
       <View className="mt-12">
-        <Text className="text-white text-3xl font-extralight mb-5">Selecciona tu nivel de inicio</Text>
+        <Text className="text-white text-3xl font-extralight mb-5">Selecciona tu nivel</Text>
         {/* <Text className="text-white text-lg mb-10">Actividad: Yoga</Text> */}
       </View>
 
@@ -88,14 +107,16 @@ const RegisterYogaLevelScreen = () => {
         {niveles.map((level, index) => (
           <TouchableOpacity
             key={index}
-            className={`flex-row items-center p-3 my-3 rounded-3x1 w-full border-2 border-black/10 rounded-2xl ${selectedLevel === level.nombre ? 'bg-black' : ''}`}
-            onPress={() => setSelectedLevel(level.nombre)}
+            className={`flex-row items-center p-3 my-3 rounded-3x1 w-full rounded-2xl bg-gray-500 shadow-2xl shadow-black`}
+            onPress={() => {setSelectedLevel(level.nombre);
+              handleNavigation(level.route as keyof RootStackParamList);  }
+            } 
           >
             <View className='flex-1'>
-              <Text className={`text-3xl font-extralight text-black mb-1 ${selectedLevel === level.nombre ? 'text-white' : 'text-black'}`}>
+              <Text className={`text-3xl font-extralight text-white mb-1`}>
                 {level.nombre}
               </Text>
-              <Text className={`text-base ${selectedLevel === level.nombre ? 'text-gray-400' : 'text-black'}`}>
+              <Text className={`text-base text-gray-300`}>
                 {level.descripcion}
               </Text>
             </View>
@@ -103,11 +124,11 @@ const RegisterYogaLevelScreen = () => {
           </TouchableOpacity>
         ))}
 
-        <TouchableOpacity
+        {/* <TouchableOpacity
           className="bg-[#595959] px-5 py-3 rounded-lg mt-6"
           onPress={handleSave}>
           <Text className="text-white text-lg font-extralight">Guardar y Volver</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     </View>
   );
