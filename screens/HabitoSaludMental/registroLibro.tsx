9ac868b54fa5b5b1b58e8a1aa583623d1ec467c4
Andres Chavez <t4t4x777@gmail.com>
2025-05-31 14:49:33 -0400

    mejora del diseno de la pantalla registro libro

--- screens/HabitoSaludMental/registroLibro.tsx
+++ screens/HabitoSaludMental/registroLibro.tsx
@@ -1,23 +1,24 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { TextInput } from 'react-native';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { auth, firestore } from '../../firebase';
 import { useState } from 'react';
-import type { RouteProp } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { useRoute } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 import CambiarHora from 'Componentes/Hora';
 import * as Notifications from 'expo-notifications';
+import { AntDesign } from '@expo/vector-icons';
+import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Alert } from 'react-native';
 
 
 type RootStackParamList = {
     RegistroLecturaDiaria: undefined;
 };
 
-type MensajeRouteProp = RouteProp<RootStackParamList, 'RegistroLecturaDiaria'>;
 type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
+
 
 const RegistroLibro = () => {
     const navigation = useNavigation<NavigationProp>();
@@ -31,6 +32,21 @@ const RegistroLibro = () => {
 
         if (!user) {
             console.log("Usuario no autenticado");
+            return;
+        }
+
+        if (titulo.trim() === '' || paginas.trim() === '') {
+            Alert.alert(
+                'Campos incompletos',
+                'Por favor, completa todos los campos.',
+                [
+                    {
+                        text: 'Entendido',
+                    },
+                ],
+                { cancelable: false }
+            );
+
             return;
         }
 
@@ -49,7 +65,7 @@ const RegistroLibro = () => {
                     minutos: minutos,
                 });
                 console.log('guardado');
-                
+
                 const ahora = new Date();
                 const horaNotificacion = new Date(ahora);
                 horaNotificacion.setHours(hora);
@@ -79,62 +95,84 @@ const RegistroLibro = () => {
                 menuRegistroLibro()
             }
         } catch (error) {
-        console.error("Error al guardar:", error);
-    }
+            console.error("Error al guardar:", error);
+        }
+    };
 
+    const menuRegistroLibro = async () => {
+        navigation.navigate('RegistroLecturaDiaria');
+    };
 
+    const handleBack = () => {
+        navigation.goBack();
+    };
 
-
-};
-
-const menuRegistroLibro = async () => {
-    navigation.navigate('RegistroLecturaDiaria');
-};
-
-return (
-    <LinearGradient
-        colors={['#0f1829', '#305bab']}
-        start={{ x: 0.5, y: 0 }}
-        end={{ x: 0.5, y: 1 }}
-        className="flex-1 justify-center items-center"
-    >
-        <View className='bg-white'>
-            <Text>Registro de Libro</Text>
-        </View>
-        <View className='bg-red-500'>
-            <TextInput
-                placeholder='Ingrese Titulo del Libro'
-                value={titulo}
-                onChangeText={setTitulo}
-            />
-        </View>
-
-        <View className='bg-green-500'>
-            <TextInput
-                placeholder='Ingrese cantidad de paginas del libro'
-                value={paginas}
-                onChangeText={setPaginas}
-                keyboardType='numeric'
-            />
-        </View>
-
-
-        <CambiarHora onHoraSeleccionada={(h, m) => {
-            setHora(h);
-            setMinutos(m);
-        }}
-        />
-
-        <TouchableOpacity
-            onPress={async () => {
-                await registrar();
-                // await programarNotificacion();
-                // menuRegistroLibro();
-            }}
+    return (
+        <KeyboardAvoidingView
+            behavior="height"
+            className="flex-1"
         >
-            <Text className='bg-white'>Guardar</Text>
-        </TouchableOpacity>
-    </LinearGradient>
-);
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <LinearGradient
+                    colors={['#000000', '#0f1828']}
+                    start={{ x: 1, y: 0 }}
+                    end={{ x: 1, y: 1 }}
+                    className="flex-1 justify-center items-center"
+                >
+                    <View className="absolute top-10 left-4 z-10">
+                        <TouchableOpacity onPress={handleBack}>
+                            <AntDesign name="arrowleft" size={30} color="white" />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View className="px-8 bg-[#202938] rounded-3xl p-6">
+                        <View className="mt-10 items-center">
+                            <Text className="text-4xl text-white">Registro de Libro</Text>
+                        </View>
+
+                        <View className='items-center mb-2 mt-3'>
+                            <Image
+                                source={require('../../assets/iconoLibro.png')}
+                                className="w-16 h-16"
+                            />
+                        </View>
+
+                        <View className='bg-white rounded-2xl mt-5'>
+                            <TextInput
+                                placeholder='Ingrese Titulo del Libro'
+                                value={titulo}
+                                onChangeText={setTitulo}
+                                returnKeyType="next"
+                            />
+                        </View>
+
+                        <View className='bg-white rounded-2xl mt-5 mb-4'>
+                            <TextInput
+                                placeholder='Ingrese cantidad de paginas del libro'
+                                value={paginas}
+                                onChangeText={setPaginas}
+                                keyboardType='numeric'
+                            />
+                        </View>
+
+                        <CambiarHora onHoraSeleccionada={(h, m) => {
+                            setHora(h);
+                            setMinutos(m);
+                        }}
+                        />
+                        <TouchableOpacity
+                            onPress={async () => {
+                                await registrar();
+                            }}
+                            className='bg-sky-300/75 items-center rounded-3xl py-1 mx-16'
+                        >
+                            <Text className='text-white text-lg'>Guardar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </LinearGradient>
+            </TouchableWithoutFeedback>
+
+        </KeyboardAvoidingView>
+    );
 };
 export default RegistroLibro;
